5c8e8c5731088c3c798e724c3d26d10508e549ee
j-jayes <j0nathanjayes@gmail.com>
2021-04-29 11:05:20 +0200

    Adding processor script

---
+++ scraped-data-processor.R
@@ -0,0 +1,55 @@
+#' ---
+#' title: "scraped-data-processor"
+#' author: "JJayes"
+#' date: "29/04/2021"
+#' output: html_document
+#' ---
+#' 
+## ----setup, include=FALSE-----------------------------------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+library(tidyverse)
+library(glue)
+
+
+#' 
+## -----------------------------------------------------------------------------------------------------------
+df <- read.csv("data/latest/ads_latest.csv")
+
+df <- df %>% 
+  as_tibble() %>% 
+  janitor::clean_names()
+
+#' 
+#' What do we need?
+#' 
+## -----------------------------------------------------------------------------------------------------------
+df <- df %>% 
+  mutate(make_model = glue("{make} {model}"),
+         price = round(price),
+         year = round(year)) %>% 
+  select(title,
+         price,
+         make_model,
+         province,
+         kilometers,
+         colour,
+         year,
+         ad_url,
+         text)
+
+#' 
+#' Processing province
+#' 
+## -----------------------------------------------------------------------------------------------------------
+df <- df %>% 
+  mutate(province = str_replace(province, "-", " "),
+         province = str_replace(province, "\\+", "-"),
+         province = str_to_title(province))
+
+#' 
+#' 
+## -----------------------------------------------------------------------------------------------------------
+write.csv(df, "data/latest/ads_latest_clean.csv")
+
+#' 
